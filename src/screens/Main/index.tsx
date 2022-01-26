e965067f08395c63a10870f7a851506a8f35f91d
joaovtmarques <jvsilvam@outlook.com>
2022-01-26 20:37:44 -0300

    first commit

---
+++ src/screens/Main/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Container, Title} from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Title>Main</Title>
+    </Container>
+  );
+}
---
+++ src/utils/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    primary: '#1A1A1A',
+    secondary: '#262626',
+
+    white: '#FFFFFF',
+
+    green: '#36AC68',
+  },
+
+  // fonts: {
+  //   text: 'SF Pro Text Regular',
+  //   regular: 'SFProDisplay-Regular',
+  // },
+};
